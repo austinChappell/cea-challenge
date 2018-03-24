b1d7163511fcc3dd1ca7141a150c4b0ff0729853
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-24 16:43:08 -0500

    add show more buttons

--- src/components/shared/Event.js
+++ src/components/shared/Event.js
@@ -6,14 +6,21 @@ import Card from './Card';
 const Event = (props) => {
   const { event } = props;
   const {
+    rsvp_limit: rsvpLimit,
     name,
     time,
+    yes_rsvp_count: rsvp,
   } = event;
   return (
     <Card>
       <div className="Event">
         <h4>{name}</h4>
         <h6>{moment(time).format('MMMM Do YYYY, h:mm a')}</h6>
+        <h6>{rsvp} going</h6>
+        <h6>{rsvpLimit - rsvp} spots remaining</h6>
+        <button>
+          Learn More
+        </button>
       </div>
     </Card>
   );

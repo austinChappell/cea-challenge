6758ae141a3b2e91ce0ad27f72e4f7843d930ebb
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-24 08:07:16 -0500

    attempting meetup auth

---
+++ src/api/api.js
@@ -0,0 +1,38 @@
+class Api {
+  authorize = (eventName, cb) => {
+    const url = `https://secure.meetup.com/oauth2/authorize?client_id=${process.env.REACT_APP_MEETUP_CONSUMER_KEY}&response_type=token&redirect_uri=${process.env.REACT_APP_SELF_URI}`;
+    return fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      console.log('RESPONSE', response);
+      return response.json();
+    }).then((results) => {
+      console.log('RESULTS', results);
+      cb(results);
+    }).catch((err) => {
+      throw err;
+    })
+  }
+
+  getMeetupData = (eventName, cb) => {
+    const url = `https://api.meetup.com/${eventName}/events`;
+    return fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_MEETUP_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      console.log('RESPONSE', response);
+      return response.json();
+    }).then((results) => {
+      console.log('RESULTS', results);
+      cb(results);
+    }).catch((err) => {
+      throw err;
+    })
+  }
+}
+
+export default Api;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+import Api from '../api/api';
+
+const api = new Api();
+
+const { authorize, getMeetupData } = api;
+
 class App extends Component {
+  componentDidMount() {
+    // getMeetupData('reactjs-dallas', this.printRes);
+  }
+
+  getData = () => {
+    authorize('reactjs-dallas', this.printRes);
+  }
+
+  printRes = (results) => {
+    console.log('RESPONSE', results)
+  }
+
   render() {
     return (
       <div className="App">
-        hello
+        <a href={`https://secure.meetup.com/oauth2/authorize?client_id=${process.env.REACT_APP_MEETUP_CONSUMER_KEY}&response_type=token&redirect_uri=${process.env.REACT_APP_MEETUP_REDIRECT_URI}`}>Authorize</a>
       </div>
     );
   }

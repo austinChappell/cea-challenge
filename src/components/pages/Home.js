0799d1c6c53c93f4b958d07111b95b2530454ef3
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-24 23:18:44 -0500

    redirect adding group name

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+const myMeetups = [
+  {
+    title: 'React',
+    url: 'ReactJS-Dallas',
+  },
+  {
+    title: 'NodeSchool',
+    url: 'Nodeschool-Dallas',
+  },
+];
+
 class Home extends Component {
   componentWillMount() {
     if (!this.props.accessToken) {
@@ -19,18 +30,34 @@ class Home extends Component {
     }
   }
 
+  storeGroupUrl = (url) => {
+    sessionStorage.setItem('groupUrl', url);
+  }
+
   render() {
     const { accessToken } = this.props;
     const meetupAuthUrl = `https://secure.meetup.com/oauth2/authorize?client_id=${process.env.REACT_APP_MEETUP_CONSUMER_KEY}&response_type=token&redirect_uri=${process.env.REACT_APP_MEETUP_REDIRECT_URI}`;
+    const groupUrl = sessionStorage.getItem('groupUrl');
 
     const link = accessToken ?
-      <Redirect to="/group" />
+      <Redirect to={`/group/${groupUrl}`} />
       : <a href={meetupAuthUrl}>Enter ReactJS Dallas</a>;
 
     return (
       <div className="Home page">
         Home Component
         {link}
+        {myMeetups.map((meetup, index) => {
+          const meetupLink = accessToken ?
+            <Redirect to={'/group/${meetup.url}'} />
+            : <a href={meetupAuthUrl} onClick={() => this.storeGroupUrl(meetup.url)}>{meetup.title}</a>;
+
+          return (
+            <div key={index}>
+              {meetupLink}
+            </div>
+          );
+        })}
       </div>
     );
   }

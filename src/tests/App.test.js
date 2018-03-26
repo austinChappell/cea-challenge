e244fed93ec507578b3a72cefea13d48788b7b3f
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-25 21:15:58 -0500

    revise app.test.js file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
 import App from '../components/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('The App Component', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+})
+;
